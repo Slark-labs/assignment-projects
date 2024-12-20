9396227682f5533baafc924c50249e4ad9a57334
Ahmad-Faiz007 <ali786goku@gmail.com>
2024-12-20 17:41:35 +0500

    declare variable and shown on console 'assignment-2 Q-1'.

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("hello World");//print "hello world" 
+const firstName = "Ahmad";//give firstName const value "Ahmad", const means it will not change after givin!!
+const lastName = "Faiz";// give him also const value like first one.
+const fullName = firstName + " " + lastName;//here i give fullName const value, 1st firstName and second is space and last one is lastName!!
+console.log(fullName);// here it will be shown on bash terminal with command "node script.js" or "console.log(fullName);" in chrome console!!

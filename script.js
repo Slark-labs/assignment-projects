82e908aa462111e46b61d57f4c184d9eedce15eb
Ahmad-Faiz007 <ali786goku@gmail.com>
2024-12-24 02:32:47 +0500

    add index.html file and change javascript file name and complete assignment Q2.part 2.

--- script.js
+++
@@ -1,4 +0,0 @@
-const firstName = "Ahmad";//give firstName const value "Ahmad", const means it will not change after givin!!
-const lastName = "Faiz";// give him also const value like first one.
-const fullName = firstName + " " + lastName;//here i give fullName const value, 1st firstName and second is space and last one is lastName!!
-console.log(fullName);// here it will be shown on bash terminal with command "node script.js" or "console.log(fullName);" in chrome console!!

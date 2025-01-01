35dc91564713806c7f0787c2a000fc3c92725bf1
Ahmad-Faiz007 <ali786goku@gmail.com>
2025-01-01 18:52:42 +0500

    assigment Arithmetic and Assignment Operators

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 // const lastName = "Faiz";// give him also const value like first one.
 // const fullName = firstName + " " + lastName;//here i give fullName const value, 1st firstName and second is space and last one is lastName!!
 // console.log(fullName);// here it will be shown on bash terminal with command "node script.js" or "console.log(fullName);" in chrome console!!
-let firstName = prompt("enter your first name");
-let lastName = prompt("enter your last name");
-alert("Welcome " +" " + firstName + " " + lastName + "!");
+
+////////////////////////////////////////////////////////
+
+// let firstName = prompt("enter your first name");
+// let lastName = prompt("enter your last name");
+// alert("Welcome " +" " + firstName + " " + lastName + "!");
+
+////////////////////////////////////////////////////////
+
+let a = 5;
+let b = 10;
+console.log(`Addition = ${a+b}`);
+console.log(`Subtraction = ${a-b}`);
+console.log(`Multiplication = ${a*b}`);
+console.log(`Division = ${a/b}`);
+
+a *=5;// a*5 =25
+console.log(a);

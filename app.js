49b813f15635b4c103958a36fcb2df924025d9ae
Ahmad-Faiz007 <ali786goku@gmail.com>
2025-01-02 20:31:19 +0500

    comparison and logical Operators

--- app.js
+++ app.js
@@ -11,12 +11,34 @@
 
 ////////////////////////////////////////////////////////
 
-let a = 5;
-let b = 10;
-console.log(`Addition = ${a+b}`);
-console.log(`Subtraction = ${a-b}`);
-console.log(`Multiplication = ${a*b}`);
-console.log(`Division = ${a/b}`);
+// let a = 5;
+// let b = 10;
+// console.log(`Addition = ${a+b}`);
+// console.log(`Subtraction = ${a-b}`);
+// console.log(`Multiplication = ${a*b}`);
+// console.log(`Division = ${a/b}`);
 
-a *=5;// a*5 =25
-console.log(a);
+// a *=5;// a*5 =25
+// console.log(a);
+
+////////////////////////////////////////////////////////////
+
+//Comparison and Logical Operators
+//1)-write a program to check if a number enetered by the user is positive, negative, or zero.
+//2)-Combine logical operators to check if the number is between 1 and 100.
+// let number = prompt("enter any number");
+// number = Number(number);
+// if (number>0) {
+//     alert("The number is positive");
+// }else if (number<0) {
+//     alert("The number is negative");
+// } else {
+//     alert("The is '0'");
+// }
+// if ( number>1 && number<100 ) {
+//     console.log("this number is between from 1 to 100");
+    
+// }
+
+/////////////////////////////////////////////////////////////
+
